1d006b86ec725da4bb0e7860df9f6bd4b9344528
peren717 <peren717@gmail.com>
2019-09-27 13:45:00 -0600

    updated spawn points

--- Time Collision/Assets/Scripts/Demo_branch/scr_controller.cs
+++ Time Collision/Assets/Scripts/Demo_branch/scr_controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class scr_controller : MonoBehaviour
 {
+    public GameObject players;
     public int itemNumToSpawn;
     public float spawnPointXMin;
     public float spawnPointXMax;
@@ -25,7 +26,7 @@ public class scr_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         foreach (GameObject obj in items)
         {
             if (obj.GetComponent<scr_orbitControl>().captured)
@@ -43,8 +44,12 @@ public class scr_controller : MonoBehaviour
         for (int i = 0; i < num; i++)
         {
 
-            Vector3 spwanPoint = new Vector3(Random.Range(spawnPointXMin, spawnPointXMax), 0.5f, Random.Range(spawnPointZMin, spawnPointZMax));
-            GameObject itemToSave = Instantiate(itemTypeToSpawn[Random.Range(0, itemTypeToSpawn.Length)], spwanPoint, Quaternion.identity);
+            Vector3 spawnPoint = new Vector3(Random.Range(spawnPointXMin, spawnPointXMax), 0.5f, Random.Range(spawnPointZMin, spawnPointZMax));
+            while ((Vector3.Distance(spawnPoint, players.transform.position) < 3))
+            {
+                spawnPoint = new Vector3(Random.Range(spawnPointXMin, spawnPointXMax), 0.5f, Random.Range(spawnPointZMin, spawnPointZMax));
+            }
+            GameObject itemToSave = Instantiate(itemTypeToSpawn[Random.Range(0, itemTypeToSpawn.Length)], spawnPoint, Quaternion.identity);
             items.Add(itemToSave);
         }
     }
