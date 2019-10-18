2711105fc5738c3f5fc0b37eeab0633635035c89
Zhuyi Liu <32209806+liuzhuyi1999@users.noreply.github.com>
2019-10-18 12:07:05 -0600

    RespawnAndAttributeFramework

---
+++ Time Collision/Assets/Scripts/ObjectAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAttribute : MonoBehaviour
+{
+    // weight is the effective health of the player which also determine their current level of size
+    // power is how much damage in terms of weight that will be dealth when the object hit the enemy player
+    public float Power;
+    public float Weight;
+
+    // returning the power value to the caller
+    public float GetPower()
+    {
+        return Power;
+    }
+
+    // returning the weight value to the caller
+    public float GetWeight()
+    {
+        return Weight;
+    }
+}
---
+++ Time Collision/Assets/Scripts/ObjectRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRespawn : MonoBehaviour
+{
+    public GameObject ObjectToRespawn;
+    // These are two variable for determine the range of offset when new respawn is created
+    public float MinOffsetX;
+    public float MaxOffsetY;
+    // These are variables that determine the respawn timer based on the min and max value
+    public float MinTimer;
+    public float MaxTimer;
+    private float CurrentTimer;
+
+    private float NewTimer()
+    {
+        return Random.Range(MinTimer, MaxTimer);
+    }
+    void OnEnable()
+    {
+        // Getting the respawn timer for the first time
+        CurrentTimer = NewTimer();
+    }
+}
