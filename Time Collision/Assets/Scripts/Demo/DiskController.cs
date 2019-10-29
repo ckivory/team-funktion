0920d50807c55b313ef544096ed9659f399a1b58
peren717 <peren717@gmail.com>
2019-10-28 19:20:31 -0600

    Update DiskController.cs

--- Time Collision/Assets/Scripts/Demo/DiskController.cs
+++ Time Collision/Assets/Scripts/Demo/DiskController.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class DiskController : MonoBehaviour
 {
     public GameObject player;
+    private List<int> inventory;
 
+    private void Start()
+    {
+       
+    }
+    private void Update()
+    {
+        
+    }
+
+    public void UpdateDisk()
+    {
+
+    }
 
 }
