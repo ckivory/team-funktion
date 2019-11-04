dd7bc211a2afbec8e1aa4503a42073f2d8bba39f
ckivory <ckivory@gmail.com>
2019-11-03 18:00:57 -0700

    Confirmed merge of orbitting code.

--- Time Collision/Assets/Scripts/Vertical Slice/VSDiskController.cs
+++ Time Collision/Assets/Scripts/Vertical Slice/VSDiskController.cs
@@ -4,7 +4,88 @@ using UnityEngine;
 
 public class VSDiskController : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject disk;
+    GameObject player;
+    List<int> inventory;
+    float diskSize;
+    List<GameObject> collectedPropPrefabs;
+    List<GameObject> diskObjects;
+    public List<int> orbitalCapacityList;
+    public List<int> orbitalRadiusList;
+    public List<float> orbitalSpeedList;
 
-    // Stub
+    private void Start()
+    {
+        player = GetComponent<GameObject>();
+        inventory = GetComponent<VSPlayerController>().inventory;
+        collectedPropPrefabs = GetComponent<VSPlayerController>().collectedPropPrefabs;
+        diskObjects = new List<GameObject>();
+    }
+    private void Update()
+    {
+
+    }
+
+    public void AddToDisk(int collectedType)
+    {
+        int tier = GetTier();
+        GameObject objectToAdd = Instantiate(collectedPropPrefabs[collectedType]);
+        objectToAdd.AddComponent<OrbitControl>();
+        objectToAdd.GetComponent<OrbitControl>().playerToFollow = this.gameObject;
+        objectToAdd.GetComponent<OrbitControl>().rotR = orbitalRadiusList[tier];
+        objectToAdd.GetComponent<OrbitControl>().rotSpeed = orbitalSpeedList[tier];
+        objectToAdd.GetComponent<OrbitControl>().type = collectedType;
+        if (diskObjects.Count > 0)
+        {
+            objectToAdd.GetComponent<OrbitControl>().timer = diskObjects[diskObjects.Count - 1].GetComponent<OrbitControl>().timer + 2*Mathf.PI / orbitalCapacityList[tier]*orbitalRadiusList[tier];
+        }
+        diskObjects.Add(objectToAdd);
+        ResizeDisk(tier);
+    }
+
+    public void RemoveFromDisk(int collectedType)
+    {
+        GameObject objectToRemove;
+        for (int i = diskObjects.Count-1; i >= 0; i--)
+        {
+            if (diskObjects[i].GetComponent<OrbitControl>().type ==  collectedType)
+            {
+                objectToRemove = diskObjects[i];
+                Object.Destroy(objectToRemove);
+                diskObjects.RemoveAt(i);
+                break;
+            }
+        }
+        ResizeDisk(GetTier());
+
+    }
+
+    public void UpdateDisk()
+    {
+       
+    }
+
+    int GetTier()
+    {
+        int n = diskObjects.Count + 1;
+        int t = 1;
+        for (int i = 0; i < orbitalCapacityList.Count; i++)
+        {
+            if (n < orbitalCapacityList[i])
+            {
+                return t;
+            }
+            else
+            {
+                n -= orbitalCapacityList[i];
+                t++;
+            }
+        }
+        return t;
+    }
+
+    void ResizeDisk(float tier)
+    {
+        disk.transform.localScale = new Vector3(5,1,5)*(1f+tier/10f);
+    }
 }
