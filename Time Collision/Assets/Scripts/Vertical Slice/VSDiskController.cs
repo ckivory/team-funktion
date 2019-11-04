c372cb76c94c01ddd7cd4326649d1f641daef48d
ckivory <ckivory@gmail.com>
2019-11-03 17:50:20 -0700

    Fixed a bug with the lightsaber not despawning after being fired.

--- Time Collision/Assets/Scripts/Vertical Slice/VSDiskController.cs
+++ Time Collision/Assets/Scripts/Vertical Slice/VSDiskController.cs
@@ -6,29 +6,5 @@ public class VSDiskController : MonoBehaviour
 {
     public GameObject player;
 
-    private float scale;
-
-    /*
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Disk collided with: " + other.gameObject.tag);
-        if(other.gameObject.CompareTag("Collectible"))
-        {
-            // Tell player to collect object.
-            Destroy(other.gameObject);
-        }
-    }
-    */
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Stub
 }
