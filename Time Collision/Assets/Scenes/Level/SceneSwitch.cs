5da08386637c18e451cf4518c6ccbeeb22673628
Zhuyi Liu <32209806+liuzhuyi1999@users.noreply.github.com>
2019-11-04 14:29:03 -0700

    LevelFix

---
+++ Time Collision/Assets/Scenes/Level/SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public Button btnStart;
+    void Start()
+    {
+        btnStart = gameObject.GetComponent<Button>();
+        btnStart.onClick.AddListener(Switch);
+    }
+    void Switch()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
