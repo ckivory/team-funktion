288751fe49c073afeb0da7c27b824043348bdaf4
Zhuyi Liu <32209806+liuzhuyi1999@users.noreply.github.com>
2019-11-04 14:38:18 -0700

    DespawnScript

---
+++ Time Collision/Assets/Prefabs/GameObject/DespawnObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnObject : MonoBehaviour
+{
+    public float DespawnTimer;
+    private float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = DespawnTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
