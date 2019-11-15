cb3600aae35d34a36904dd67f5ca93678f1a8306
Zhuyi Liu <32209806+liuzhuyi1999@users.noreply.github.com>
2019-11-15 00:44:45 -0700

    Reorganize and some stats change

---
+++ Time Collision/Assets/Scripts/Misc/SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public Button btnStartGame;
+    void OnEnable()
+    {
+        btnStartGame = gameObject.GetComponent<Button>();
+        btnStartGame.onClick.AddListener(StartGame);
+    }
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
