91a22f83a227816a268e63596ba9792159bc36d9
Zhuyi Liu <>
2019-11-29 16:06:22 -0700

    Adjustment of Object Spawn Behavior

---
+++ Time Collision/Assets/Scripts/Misc/FiredBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiredBehavior : MonoBehaviour
+{
+    public float xRotate;
+    public float yRotate;
+    public float zRotate;
+    public bool goLeft = false;
+    public bool goForward = false;
+    private Rigidbody rb;
+    public bool spin=false;
+    private float timer = 0.2f;
+    private float speed = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        gameObject.transform.Rotate(xRotate, yRotate, zRotate);
+    }
+    void Update()
+    {
+        if (spin)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                speed -= Time.deltaTime * 180;
+            }
+            if (speed > 0)
+            {
+                gameObject.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+            }
+            else spin = false;
+        }
+        if (goLeft)
+        {
+            //rb.AddTorque(-transform.right * Random.Range(minForce, maxForce));
+            gameObject.transform.Rotate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (goForward)
+        {
+            //rb.AddTorque(-transform.forward * Random.Range(minForce, maxForce));
+            gameObject.transform.Rotate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
--- Time Collision/Assets/Scripts/Misc/ObjectAttributes.cs
+++ Time Collision/Assets/Scripts/Misc/ObjectAttributes.cs
@@ -7,8 +7,8 @@ public class ObjectAttributes : MonoBehaviour
     public GameObject whoFired;
     public int propNum;
 
-    private static List<float> massList = new List<float> {1f, 1f, 5f, 3f, 1f, 2f, 1f, 0.5f, 2f, 2f};
-    public static List<float> damageList = new List<float> {2f, 2f, 10f, 6f, 10f, 4f, 7f, 9f, 5f, 4f};
+    private static List<float> massList = new List<float> {1f, 1f, 5f, 3f, 1f, 2f, 1f, 0.5f, 2f, 2f, 8f};
+    public static List<float> damageList = new List<float> {2f, 2f, 10f, 6f, 10f, 4f, 7f, 9f, 5f, 4f, 15f};
 
     public static float getMass(int p)
     {
