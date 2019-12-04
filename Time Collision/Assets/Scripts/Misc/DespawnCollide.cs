5e915fda664e89e9c0331385e8cd178048b1f903
Zhuyi Liu <>
2019-12-04 01:24:00 -0700

    Add destroy function to particle

--- Time Collision/Assets/Scripts/Misc/DespawnCollide.cs
+++ Time Collision/Assets/Scripts/Misc/DespawnCollide.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class DespawnCollide : MonoBehaviour
 {
     public GameObject particle;
-    void OnCollisionEnter(Collision col)
-    {
-        Debug.Log("Collision detected.");
-        Destroy(gameObject);
-    }
     void OnDestroy()
     {
         Instantiate(particle, transform.position, transform.rotation);
--- Time Collision/Assets/Scripts/Misc/ParticleRemove.cs
+++ Time Collision/Assets/Scripts/Misc/ParticleRemove.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathSequece : MonoBehaviour
+public class ParticleRemove : MonoBehaviour
 {
+    private ParticleSystem ps;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ps = gameObject.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!ps.IsAlive())
+        {
+            Destroy(gameObject);
+        }
     }
 }
