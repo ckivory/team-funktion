cf7b7d197f6d3b7d16f633702d16632725c790da
ckivory <ckivory@gmail.com>
2019-12-05 21:03:09 -0700

    Added Reaper GameObject to allow users to close the game by double-tapping the Escape key. Added blob shadows underneath players to allow for lining up shots more easily.

---
+++ Time Collision/Assets/Scripts/Publisher Demo/ReaperController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ReaperController : MonoBehaviour
+{
+    public float interval = 0.2f;
+    private float timer;
+
+    private void CloseGame()
+    {
+        #if UNITY_EDITOR
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+        #endif
+
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        timer = 0f;
+    }
+    
+    void Update()
+    {
+        // Double-tab escape to close game
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(timer > 0f)
+            {
+                CloseGame();
+            }
+            else
+            {
+                timer = interval;
+            }
+        }
+
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
